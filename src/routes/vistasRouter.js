2e5b1842ec1164e9da91811e6166e068995e544d
Erland Ruiz <erlandruiz@hotmail.com>
2023-12-04 10:55:29 -0500

    Primer commit

---
+++ src/routes/vistasRouter.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+export const router=Router()
+
+router.get('/',(req,res)=>{
+
+    
+
+    res.status(200).render('home')
+})
+router.get('/chat',(req,res)=>{
+
+    
+
+    res.status(200).render('chat')
+})
